f9bdd9f6ee3819688419b6066a80af30873999b3
wejden <wejden.cherbib@gfi-tunisie.com>
2021-03-09 21:20:42 +0100

    integrated authentification page

---
+++ src/components/Logout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Logout.css'
+const Logout = () =>{
+return(
+    <div className="logout">
+        <h1>Welcome <span className="user__name">wejden</span></h1>
+        <button className="logout__button">Logout</button>
+    </div>
+)
+}
+export default Logout
